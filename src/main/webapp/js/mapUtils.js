1aa363294c4b6d921a28eb44f84115c7d7580236
kimhj <kimhj@gaia3d.com>
2019-03-14 11:20:10 +0900

    prototype으로 코드 수정

--- src/main/webapp/js/mapUtils.js
+++ src/main/webapp/js/mapUtils.js
@@ -4,17 +4,23 @@ var MapUtils = function(map) {
 		throw new Error("New 를 통해 생성 하십시오.");
 	}
 
-	var map = map;
-	var view = map.getView();
+	this.map = map;
+	this.view = map.getView();
+};
 
-	this.getCurProj = function() {
-		return view.getProjection();
-	};
+MapUtils.prototype = {
+	contructor: MapUtils,
 
-	this.getLayerById = function(layerId) {
+	// 현재 좌표계 조회 (HHI.Utils.getCurProj())
+	getCurProj: function() {
+		return this.view.getProjection();
+	},
+
+	// ID를 통해 레이어 찾기 (HHI.Utils.getLayerById('base_layer'))
+	getLayerById: function(layerId) {
 		var layer = null;
 		if(layerId){
-			var layers = map.getLayers().getArray();
+			var layers = this.map.getLayers().getArray();
 			for(var i in layers){	 // 브라우저 호환성 - ie6~, chrome
 				if(layers[i].get('id') === layerId){
 					layer = layers[i];
@@ -23,6 +29,5 @@ var MapUtils = function(map) {
 			}
 		}
 		return layer;
-	};
-
-};
+	}
+}
